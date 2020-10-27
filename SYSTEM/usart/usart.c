d91ffe924a57deab31d035cc766c0eb1c11f6018
linf <elmo_lf@126.com>
2020-10-27 09:28:45 +0800

    [加入443/315Mhz遥控控制]

--- SYSTEM/usart/usart.c
+++ SYSTEM/usart/usart.c
@@ -85,19 +85,45 @@ void uart_init(u32 bound)
 	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
 	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
 
-  USART_Init(USART1, &USART_InitStructure); //初始化串口1
-  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启串口接受中断
-  USART_Cmd(USART1, ENABLE);                    //使能串口1 
-
+    USART_Init(USART1, &USART_InitStructure); //初始化串口1
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启串口接受中断
+    USART_Cmd(USART1, ENABLE);                    //使能串口1 
 }
-
+u8 remote_val_rec[3];
+u8 remote_val_get=0;
 void USART1_IRQHandler(void)                	//串口1中断服务程序
 {
 	u8 Res;
-	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+    static u8 unpack_stage=0;
+	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)  
 	{
         Res =USART_ReceiveData(USART1);	//读取接收到的数据	 
     } 
+    if ((Res==0xFD)&&(unpack_stage==0)&&(remote_val_get==0))
+    {
+        unpack_stage++;
+    }
+    if (unpack_stage>=1)
+    {
+        if (unpack_stage>1)
+        {
+            if (unpack_stage<5)
+            {
+                remote_val_rec[unpack_stage-2]=Res;
+            }
+            if (unpack_stage==6)
+            {
+                if (Res==0xDF)
+                {
+                    remote_val_get=1;
+                    unpack_stage=0;
+                }
+                unpack_stage=0;
+            }
+        }
+        unpack_stage++;
+    }
+    
 } 
 #endif	
 
--- USER/main.c
+++ USER/main.c
@@ -15,8 +15,45 @@
 #include "rtc.h"
 #include "timer.h"
 #include "aircondit.h"
+#include "temperature.h"
 u8 test_commd;
-
+#define         HDC_1080_ADD                            0x40
+#define         Configuration_register_add              0x02
+#define         Temperature_register_add                0x00
+#define         Humidity_register_add                   0x01
+void TEMPERATURE_init()
+{
+    uint16_t config_reg_value=0x1000;
+	uint8_t data_send[2];
+    IIC_Init();
+    
+    data_send[0]= (config_reg_value>>8);
+	data_send[1]= (config_reg_value&0x00ff);
+    
+    IIC_Start();
+    IIC_Send_Byte(0x80);
+    if (IIC_Wait_Ack())
+    {
+        while(1)
+        {
+            IIC_SDA=1;
+            IIC_SCL=1;
+            delay_ms(1000);
+            IIC_SDA=0;
+            IIC_SCL=0;
+            delay_ms(1000);
+            
+        }
+    }
+    IIC_Send_Byte(data_send[0]);
+    IIC_Wait_Ack();
+    IIC_Send_Byte(data_send[1]);
+    IIC_Wait_Ack();
+    IIC_Stop();
+    delay_us(10);
+	//IIC_Write_One_Byte(Configuration_register_add,data_send[0]);
+    //IIC_Write_One_Byte(Configuration_register_add,data_send[1]);
+}
 void check_event()
 {
     
@@ -37,18 +74,99 @@ void check_event()
         AIRCOND_send_commd(AIRCOND_MODE_POWEROFF);
     }
 }
-
+extern u8 remote_val_rec[3];
+extern u8 remote_val_get;
+u8 REMOTE_commd[3][7]={{0xFD,0x01,0x91,0x80,0xE1,0x90,0xDF},{0xFD,0x01,0xFE,0x1E,0xB1,0x50,0xDF},{0xFD,0x01,0x48,0x13,0x08,0x50,0xDF}};
+void check_remote()
+{
+    if (remote_val_get)
+    {
+        if ((remote_val_rec[0]==0x91)&&(remote_val_rec[1]==0x80)&&(remote_val_rec[2]==0xD1))
+        {
+            for (uint8_t i = 0; i < 7; i++)
+            {
+                USART_SendData(USART1, REMOTE_commd[0][i]);
+                while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+            }
+            delay_ms(1200);
+            for (uint8_t i = 0; i < 7; i++)
+            {
+                USART_SendData(USART1, REMOTE_commd[1][i]);
+                while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+            }
+            delay_ms(1200);
+            for (uint8_t i = 0; i < 7; i++)
+            {
+                USART_SendData(USART1, REMOTE_commd[2][i]);
+                while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+            }
+        }
+        if ((remote_val_rec[0]==0xFE)&&(remote_val_rec[1]==0x1E)&&(remote_val_rec[2]==0xA1))
+        {
+            for (uint8_t i = 0; i < 7; i++)
+            {
+                USART_SendData(USART1, REMOTE_commd[0][i]);
+                while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+            }
+            delay_ms(1200);
+            for (uint8_t i = 0; i < 7; i++)
+            {
+                USART_SendData(USART1, REMOTE_commd[1][i]);
+                while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+            }
+            delay_ms(1200);
+            for (uint8_t i = 0; i < 7; i++)
+            {
+                USART_SendData(USART1, REMOTE_commd[2][i]);
+                while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+            }
+        }
+        remote_val_get=0;
+    }
+}
+void LAMP_init()
+{
+    ;
+}
+void REMOTE_init()
+{
+    ;
+}
+u8 iic_rec[4]={0};
+void TEMPERATURE_get()
+{
+    IIC_Start();   
+    IIC_Send_Byte(0x81);   
+    IIC_Wait_Ack();
+    IIC_Send_Byte(0x00);
+    IIC_Wait_Ack(); 
+    delay_ms(15);
+    iic_rec[0]=IIC_Read_Byte(0); 
+    iic_rec[1]=IIC_Read_Byte(0);
+    iic_rec[2]=IIC_Read_Byte(0);
+    iic_rec[3]=IIC_Read_Byte(0);
+    IIC_Stop();
+}
+u8 temp_start=0;
 int main(void)
 {
     delay_init();
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     uart_init(9600);
     LED_Init();
+    LAMP_init();
     RTC_Init();
+    //TEMPERATURE_init();
     AIRCOND_init();
     while (1)
     {
         check_event();
+        check_remote();
+        if (temp_start)
+        {
+            TEMPERATURE_get();
+             temp_start=0;
+        }
         delay_ms(500);
     }
 }
